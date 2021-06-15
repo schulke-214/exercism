dbc0013b80d941150c0a3c6f55a8af6af0b79945
Maximilian Schulke <hello@maximilianschulke.com>
2021-06-15 14:41:05 +0200

    Update rust gigasecond solution

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,8 @@
 use chrono::{DateTime, Duration, Utc};
-use std::ops::Add;
+
+const SECOND_OFFSET: i64 = 1_000_000_000;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start + Duration::seconds(1_000_000_000)
+    start + Duration::seconds(SECOND_OFFSET)
 }
