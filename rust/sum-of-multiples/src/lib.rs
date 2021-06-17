e6c5db1e6a72f9e2ce971a94427ad5be46543c39
Maximilian Schulke <hello@maximilianschulke.com>
2021-06-18 00:24:56 +0200

    Add the solution for the sum of multiples exercise

---
+++ rust/sum-of-multiples/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
+    let valid_factors = factors.iter().filter(|f| **f > 0);
+
+    (1..limit)
+        .filter(|x| valid_factors.clone().any(|f| x % f == 0))
+        .sum()
+}
