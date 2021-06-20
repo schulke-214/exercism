424d90263aadfd4860d721cf8de2b2ac138c099d
Maximilian Schulke <hello@maximilianschulke.com>
2021-06-20 22:04:11 +0200

    Optimize the beer song solution

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -8,9 +8,7 @@ pub fn verse(n: u32) -> String {
             next = n - 1,
             next_bottle_pluralized = { if n - 1 > 1 { "bottles" } else { "bottle" } }
         ),
-        1 => format!(
-            "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-        ),
+        1 => "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string(),
         0 => String::from(
             "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
         )
@@ -20,7 +18,7 @@ pub fn verse(n: u32) -> String {
 pub fn sing(start: u32, end: u32) -> String {
     (end..=start)
         .rev()
-        .map(|n| verse(n))
+        .map(verse)
         .intersperse("\n".to_owned())
         .collect()
 }
