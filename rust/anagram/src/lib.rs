4cbde3693577d5ef4826af497ed1595ce16335fa
Maximilian Schulke <hello@maximilianschulke.com>
2021-06-20 16:04:07 +0200

    Add the rust solution for detecting anagrams

---
+++ rust/anagram/src/lib.rs
@@ -0,0 +1,23 @@
+use std::collections::HashSet;
+
+fn is_anagram(word: &str, candidate: &str) -> bool {
+    fn to_sorted_vec(s: &str) -> Vec<String> {
+        let mut v = s
+            .chars()
+            .map(|c| c.to_lowercase().to_string())
+            .collect::<Vec<String>>();
+        v.sort();
+        v
+    }
+
+    to_sorted_vec(word).eq(&to_sorted_vec(candidate))
+}
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    possible_anagrams
+        .iter()
+        .filter(|a| is_anagram(word, a))
+        .filter(|a| a.to_lowercase() != word.to_lowercase())
+        .map(|a| *a)
+        .collect()
+}
